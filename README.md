580300c21102500cf397587f57d511ecaeea2d92
Carl Hörberg <carl.hoerberg@gmail.com>
2013-05-20 19:07:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+This sample app shows how to connect with [mosquitto](https://pypi.python.org/pypi/mosquitto/) to [CloudMQTT](http://www.cloudmqtt.com) and both publish and subscribe messages. 
+
+    $ pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,47 @@
+import mosquitto, os, urlparse
+
+# Define event callbacks
+def on_connect(mosq, obj, rc):
+    print("rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mosq, obj, level, string):
+    print(string)
+
+mqttc = mosquitto.Mosquitto()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+# Uncomment to enable debug messages
+#mqttc.on_log = on_log
+
+# Parse CLOUDMQTT_URL (or fallback to localhost)
+url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://localhost:1883')
+url = urlparse.urlparse(url_str)
+
+# Connect
+mqttc.username_pw_set(url.username, url.password)
+mqttc.connect(url.hostname, url.port)
+
+# Start subscribe, with QoS level 0
+mqttc.subscribe("hello/world", 0)
+
+# Publish a message
+mqttc.publish("hello/world", "my message")
+
+# Continue the network loop, exit when an error occurs
+rc = 0
+while rc == 0:
+    rc = mqttc.loop()
+print("rc: " + str(rc))
